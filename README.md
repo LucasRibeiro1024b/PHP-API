2794d242209b27095dfd5a594fa617f06eea45f1
Ribeiro <lucasribeiro1024b@gmail.com>
2022-11-08 23:17:13 -0300

    added user route

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PHP API
 
+Let’s have a look at the following structure.
+
+```
+├── src/
+│   ├── Controller
+│   │   └── Api
+│   │       ├── BaseController.php
+│   │       └── UserController.php
+│   ├── Model
+│   │   ├── Database.php
+│   │   └── UserModel.php
+│   └── inc
+│       ├── bootstrap.php
+│       └── config.php
+└── index.php
+```
+
+- **index.php**: the entry-point of our application. It will act as a front-controller of our application.
+- **inc/config.php**: holds the configuration information of our application. Mainly, it will hold the database credentials.
+- **inc/bootstrap.php**: used to bootstrap our application by including the necessary files.
+- **Model/Database.php**: the database access layer which will be used to interact with the underlying MySQL database.
+- **Model/UserModel.php**: the User model file which implements the necessary methods to interact with the users table in the MySQL database.
+- **Controller/Api/BaseController.php**: a base controller file which holds common utility methods.
+- **Controller/Api/UserController.php**: the User controller file which holds the necessary application code to entertain REST API calls.
+
---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Lucas\PhpApi\Controller\Api;
+
+use Lucas\PhpApi\Model\UserModel; 
+use Error;
+
+class UserController extends BaseController
+{
+    /**
+     * "/user/list" Endpoint - Get list of users
+     */
+    public function listAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+ 
+        if (strtoupper($requestMethod) == 'GET') {
+            try {
+                $userModel = new UserModel();
+ 
+                $intLimit = 10;
+                if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                    $intLimit = $arrQueryStringParams['limit'];
+                }
+ 
+                $arrUsers = $userModel->getUsers($intLimit);
+                $responseData = json_encode($arrUsers);
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+
+    /**
+     * "/user/addUser" Endpoint - Set a new user
+     */
+    public function addUserAction() {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+ 
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+                $userModel = new UserModel();
+                $arrUser = array(
+                    $_POST['username'],
+                    $_POST['user_email']
+                );
+                // $arrUsers = $userModel->getUsers($intLimit);
+                // $responseData = json_encode($arrUsers);
+                $responseData = json_encode($arrUser);
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+}
